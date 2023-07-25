f6c047fc89ff7f66ed5838a4209c9d520c1c7df7
Cassidy <cassidy.vance@hotmail.com>
2023-07-24 19:36:47 -0700

    added packages to switch db

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './store/authContext';
+import { initializeApp } from 'firebase/app'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,6 +17,19 @@ root.render(
     </AuthContextProvider>
   </React.StrictMode>
 );
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA8AEseCrxeUnxApxCKIu9-_MhlqV5mm0c",
+  authDomain: "blurb-81b30.firebaseapp.com",
+  databaseURL: "https://blurb-81b30-default-rtdb.firebaseio.com",
+  projectId: "blurb-81b30",
+  storageBucket: "blurb-81b30.appspot.com",
+  messagingSenderId: "1044281537991",
+  appId: "1:1044281537991:web:3bb7d49b93618cbf8c55e2",
+  measurementId: "G-RNXQNSNXDX"
+};
+
+const app = initializeApp(firebaseConfig);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
